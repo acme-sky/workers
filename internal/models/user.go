cdcb8bcad2a5cdc35c37d4db8d7467b08099f747
Santo Cariotti <santo@dcariotti.me>
2024-05-23 14:02:34 +0200

    Fix typo on column

--- internal/models/user.go
+++ internal/models/user.go
@@ -9,6 +9,6 @@ type User struct {
 	Username           string  `gorm:"column:username" gorm:"uniqueIndex"`
 	Email              string  `gorm:"column:email" gorm:"uniqueIndex"`
 	Password           string  `gorm:"column:password"`
-	Address            *string `gorm:"colum:address;null"`
-	ProntogramUsername *string `gorm:"colum:prontogram_username;null"`
+	Address            *string `gorm:"column:address;null"`
+	ProntogramUsername *string `gorm:"column:prontogram_username;null"`
 }
